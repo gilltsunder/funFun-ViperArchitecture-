211e42f9e2b0a32e656ddfc04d305d9da7df11dd
Vlad Tretiak <vtretyak@teamworkretail.com>
2021-05-05 11:48:33 +0300

    Initial Commit

---
+++ funFun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  funFun
+//
+//  Created by Vlad Tretiak on 05/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
